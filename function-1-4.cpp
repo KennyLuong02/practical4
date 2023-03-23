14ae4c4ddea5f55bc6b1b24bc3b9a3a7dc3ce865
Dai Luong <luong@DESKTOP-INLVFFC.localdomain>
2023-03-23 13:31:09 +1030

    Fixed an error

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -5,33 +5,8 @@
 using namespace std;
 
 PersonList shallowCopyPersonList(PersonList pl) {
-    // Original struct
-    int n = 5;
-    for (int i = 0; i < n; i++) {
-        cout << pl.people[i].name << " " << pl.people[i].age << endl;
-    }
 
-    cout << pl.numPeople << endl;
-    cout << endl;
-
-    //New struct
-    PersonList p2;
-
-    p2 = pl;
-    // p2.numPeople = n;
-
-    // for (int i = 0; i < n; i++) {
-    //     p2.people[i].name = &pl.people[i].name;
-    //     p2.people[i].age = pl.people[i].age;
-    // }
-
-    for (int i = 0; i < n; i++) {
-        cout << p2.people[i].name << " " << p2.people[i].age << endl;
-    }
-
-    cout << p2.numPeople << endl;
-
-    // delete[] p2.people;
+    PersonList p2 = pl;
     
     return p2;
 }
